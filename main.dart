7935397ec9a091af9e089e01aed0c25d0a91a926
Ramyasri0612 <ramyasrimuchkoor06@gmail.com>
2025-09-03 12:05:56 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      home: ResponsiveHome(),
+    );
+  }
+}
+class ResponsiveHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+  double screenWidth=MediaQuery.of(context).size.width;
+  double screenHeight=MediaQuery.of(context).size.height;
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('MediaQuery Responsive UI'),
+    ),
+    body:Center(child: Container(width: screenWidth *0.8,height: screenWidth *0.4,color: Colors.blue,
+    child: Center(
+    child:Text(
+      screenWidth > 600? 'Large Screen Layout':'Small Screen Layout',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: screenWidth > 600  ? 24:16,
+      ),
+    ),
+    ),
+    ),
+    ),
+    );
+  }
+}
+  
